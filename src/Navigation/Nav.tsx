0a0b4fb4762b2ec085ea631772dcab360ede4708
ahmadelbaz <ahmeed.elbaz@gmail.com>
2025-12-26 20:13:15 +0200

    feat: implement theme toggle button in Nav component; remove deprecated settings from tsconfig files

--- src/Navigation/Nav.tsx
+++ src/Navigation/Nav.tsx
@@ -1,7 +1,8 @@
+import { Moon, Sun } from "lucide-react";
 import { CiShoppingCart } from "react-icons/ci";
 import { GoPerson } from "react-icons/go";
 import { GrFavorite } from "react-icons/gr";
-import { ThemeToggle } from "react-shadcn-ui";
+import { Button, useTheme } from "react-shadcn-ui";
 import type { FilterType } from "../App";
 import SearchBar from "../components/SearchBar";
 
@@ -10,12 +11,23 @@ interface Params {
 }
 
 export default function Nav({ setFilteredProducts }: Params) {
+  const theme = useTheme();
+
+  const changeTheme = () => {
+    if (theme.theme === "dark") {
+      theme.setTheme("light");
+    } else {
+      theme.setTheme("dark");
+    }
+  };
+
   return (
     <nav className="flex justify-between mb-12 gap-2">
       <SearchBar setFilteredProducts={setFilteredProducts} />
       <section className="flex gap-3 items-center flex-col sm:flex-row">
-        {/* <Button variant="ghost">Dark</Button> */}
-        <ThemeToggle />
+        <Button variant="outline" onClick={changeTheme}>
+          {theme.theme === "dark" ? <Sun /> : <Moon />}
+        </Button>
         <GrFavorite size={20} />
         <CiShoppingCart size={30} />
         <GoPerson size={20} />
